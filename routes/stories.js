2df86827b552a770e6c936f99f91345b882de6b3
Anisha <anisha.0612@live.com>
2020-07-29 11:05:06 -0400

    remove favicon

--- routes/stories.js
+++ routes/stories.js
@@ -25,9 +25,8 @@ router.get("/", ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({ status: "public" })
       .populate("user")
-      .sort({ createdAt: "desc" })
       .lean();
-    res.render("stories/index", { stories, user: req.user });
+    res.render("stories/index", { stories });
   } catch (err) {
     console.error(err);
     res.render("errors/500");
